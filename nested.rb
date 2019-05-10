b10749584ca06f0e547a6ad35cd6add1305220a5
yana.loginova <yana.loginova@cix.csi.cuny.edu>
2019-05-10 09:02:07 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -119,14 +119,7 @@ def adding_to_dennis
  		{
         :grace_hopper => {
           :known_for => "COBOL",
-          :languages => ["COBOL", "FORTRAN"]
-        },
-        :alan_kay => {
-          :known_for => "Object Orientation",
-          :languages => ["Smalltalk", "LISP"]
-        },
-        :dennis_ritchie => {
-          :known_for => "Unix",
+          
           :languages => ["C"]
         }
      }
