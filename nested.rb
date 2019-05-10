b0e28754389606bf0ab748a44ed91530009b0d32
yana.loginova <yana.loginova@cix.csi.cuny.edu>
2019-05-10 09:02:52 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -119,10 +119,18 @@ def adding_to_dennis
  		{
         :grace_hopper => {
           :known_for => "COBOL",
-          
+          :languages => ["COBOL", "FORTRAN"]
+        },
+        :alan_kay => {
+          :known_for => "Object Orientation",
+          :languages => ["Smalltalk", "LISP"]
+        },
+        :dennis_ritchie => {
+          :known_for => "Unix",
           :languages => ["C"]
         }
      }
-programmer_hash[:dennis_ritchie][:languages]<<"Assembly"
+programmer_hash[:dennis_ritchie]
+[:languages]<<"Assembly"
 programmer_hash
 end
